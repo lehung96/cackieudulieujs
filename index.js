64bcbe8bc306e006e60545cdf402307f58f4ab2e
hùng lê <levanhung96.hh@mail.com>
2022-08-24 15:01:36 +0700

    first name

---
+++ index.js
@@ -0,0 +1,35 @@
+var number = 15;
+
+console.log(number);
+
+
+var firstName =  "Lê Hùng";
+
+console.log(typeof firstName);
+
+
+
+var arrTest = ['text', 999, firstName, [2,8,9]];
+console.log(arrTest);
+
+
+var arrNumber = [12, 10 ,9, 8];
+// var vitricuoi = arrTest.length -1
+
+console.log("vitricuoi::::",arrTest[arrTest.length -1 ][0]);
+
+
+
+var objectStudent = {
+    id: "0909090",
+    hoTen: "alex",
+    phoneNumber: 090900,
+    friend: ["quy", "phu", "son" ],
+    gioiThieu: "info",
+    giaDinh: {diaChi: "Hà Nội", phuongTien: "xe may"}
+}
+
+
+var bien = 'que'
+
+console.log(objectStudent[bien]);
